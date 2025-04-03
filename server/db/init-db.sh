e7a4bbab5f60481aa1ac4d2f1c9a602520195c3f
Faiz Marsad <faizmarsad@gmail.com>
2025-04-02 22:34:19 -0600

    chore(db): secure env variables and finalize initial DB setup

--- server/db/init-db.sh
+++ server/db/init-db.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+set -a
+source ../../.env
+set +a
 
-DB_HOST=localhost
-DB_PORT=5432
-DB_USER=admin
-DB_NAME=nrs_platform
+export PGPASSWORD=$POSTGRES_PASSWORD
 
+echo "Running schema.sql..."
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f schema.sql
 
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f schema.sql
-
-
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f seed.sql
+echo "Running seed.sql..."
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f seed.sql
